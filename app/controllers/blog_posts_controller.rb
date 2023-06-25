ac81e3867f12bd5801fbf88c3b395eab562d97e8
Taimoor Hussain <taimoorhussain985@gmail.com>
2023-06-26 00:25:55 +0500

    Resolve Add New Blog and Add TailwindCSS

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -17,6 +17,7 @@ class BlogPostsController < ApplicationController
   end
 
   def create
+    @blog_post = BlogPost.new(blog_post_params)
     if @blog_post.save
       redirect_to @blog_post
     else
