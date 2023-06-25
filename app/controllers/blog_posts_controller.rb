4bd4f9772ffba8ac7e17be3b061619b916e3ab2c
Taimoor Hussain <taimoorhussain985@gmail.com>
2023-06-25 17:35:18 +0500

    Delete Blog And Code Refracting

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -1,10 +1,12 @@
 class BlogPostsController < ApplicationController
+  before_action :set_blog_post, only: [:show, :edit, :update, :destroy]
+  # before_action :set_blog_post, except: [:index, :new, :create]
+
   def index
     @blog_posts = BlogPost.all
   end
 
   def show
-    @blog_post = BlogPost.find(params[:id])
     rescue ActiveRecord::RecordNotFound
     redirect_to root_path
   end
@@ -14,7 +16,7 @@ class BlogPostsController < ApplicationController
   end
 
   def create
-    @blog_post = BlogPost.new(blog_post_params)
+
     if @blog_post.save
       redirect_to @blog_post
     else
@@ -23,12 +25,10 @@ class BlogPostsController < ApplicationController
   end
 
   def edit
-    @blog_post = BlogPost.find(params[:id])
   end
 
 
   def update
-    @blog_post = BlogPost.find(params[:id])
       if @blog_post.update(blog_post_params)
         redirect_to @blog_post
       else
@@ -36,13 +36,24 @@ class BlogPostsController < ApplicationController
       end
   end
 
+def destroy
+
+   @blog_post.destroy
+  redirect_to root_path
+end
+
+
   private
   def blog_post_params
     params.require(:blog_post).permit(:title, :body)
   end
 
-
-
-
-
+def set_blog_post
+  @blog_post = BlogPost.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    redirect_to root_path
 end
+
+
+end # This is for clas
+
